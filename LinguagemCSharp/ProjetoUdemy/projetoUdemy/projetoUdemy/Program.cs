7fe89d2b12b3e1286d0b3d90880f3657a61d49c1
GabrielColdebella <gabriel.henrique@mail.uft.edu.br>
2024-11-28 13:49:54 -0300

    4171 Alterações em diversas pastas

---
+++ LinguagemCSharp/ProjetoUdemy/projetoUdemy/projetoUdemy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    internal class Program
+    {
+        static float soma(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[,] arrayBraba = { {1, 2}, {3, 4}};
+            foreach (int i in arrayBraba)
+            {
+                Console.WriteLine($"bagulho atual: {i}");
+            }
+            Console.WriteLine(soma(5, 10));
+        }
+    }
+}
+
